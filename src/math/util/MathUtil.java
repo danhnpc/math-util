2844477da91f13d970ba7418ed2729512f1815e4
danhnpc <visugar2000@gmail.com>
2020-11-12 08:36:32 +0700

    1st upload

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+import fu.util.MathUtility;
+
+/**
+ *
+ * @author ASUS
+ */
+public class MathUtil {
+    public static void main(String[] args) {
+        //2.test ham = mat
+        System.out.println("5! : " + MathUtility.getFactorial(5));
+        // expected: 120
+    }
+    //Code viet ra thi Developer phai test truoc cai da
+    //1. TDD, Test Driven Development
+    //Viet dan khung cua ham getFactorial(int n)
+    //sau do dung ham test()
+    //chay thu coi xanh đỏ, đỏ là lỗi, xanh là ổn
+    // cứ liên tục xanh đỏ như thế trong quá trình viết code
+    //và sửa coded, ng ta gọi là lập trình hướng theo thỏa mãn
+    //việc test , tdd
+    
+    //2. sout(hàm kèm data) để coi kq ra sao, có đúng như mình kỳ vọng
+    // nó phải trả về như thế hay kh -> xem kết quả test=mắt
+}
