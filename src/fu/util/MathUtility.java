fe5c14eabd7821a6416c83f7151afb6c18e4dd6a
danhnpc <visugar2000@gmail.com>
2020-11-19 08:35:02 +0700

    use recursion and GREEN again

--- src/fu/util/MathUtility.java
+++ src/fu/util/MathUtility.java
@@ -18,19 +18,33 @@ public class MathUtility {
     //hàm tính n giai thừa . n>=0 && n <=20
     // nếu quá 20 tràn kiểu long
     //0! = 1; 1!= 1; n!= 1.2.3...n
-    public static long getFactorial(int n) {
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be >= 0 and <= 20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1; //0! = 1! = 1
+//        }
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+        public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be >= 0 and <= 20");
         }
         if (n == 0 || n == 1) {
             return 1; //0! = 1! = 1
         }
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
+        return n * getFactorial(n - 1); //gọi lại chính mình với quy mô nhỏ hơn
+        //5! = 5 * 4!
+        //4! = 4 * 3!
+        //3! = 3 * 2!
+        //2! = 2 * 1!
+        //n! = n * (n-1)!
+    }// chơi lớn kh thèm check ở local, xem trực tiếp github
 }
 
 //Math.sqrt(), .pow(a,b) a^b, .abs() tri truyet doi
